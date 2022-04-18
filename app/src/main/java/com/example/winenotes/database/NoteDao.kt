9eff3c4e19b65f09b219db6a0c7d0415356ebfa5
Bobby <bobbyray.reid@gmail.com>
2022-04-18 12:21:00 -0500

    Database package init

---
+++ app/src/main/java/com/example/winenotes/database/NoteDao.kt
@@ -0,0 +1,18 @@
+package com.example.winenotes.database
+
+import androidx.room.*
+
+@Dao
+interface NoteDao {
+
+    @Insert
+    fun addNote(note : Note) : Long
+
+    @Update
+    fun updateNote(note : Note)
+
+    @Delete
+    fun deleteNote(note : Note)
+
+
+}
