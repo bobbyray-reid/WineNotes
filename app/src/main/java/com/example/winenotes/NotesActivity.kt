2c88de775c821e20797c06adbdde290b3f17358d
Bobby <bobbyray.reid@gmail.com>
2022-04-18 14:38:56 -0500

    NotesActivity xml setup, onbackpressed initial setup

--- app/src/main/java/com/example/winenotes/NotesActivity.kt
+++ app/src/main/java/com/example/winenotes/NotesActivity.kt
@@ -2,17 +2,19 @@ package com.example.winenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.winenotes.databinding.ActivityMainBinding
+import com.example.winenotes.databinding.ActivityNotesBinding
 
 class NotesActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityNotesBinding
 
     private var purpose : String? = ""
     private var noteId : Long = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityNotesBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val intent = getIntent()
@@ -24,6 +26,19 @@ class NotesActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
+        val title = binding.titleEditText.text.toString().trim()
+        if(title.isEmpty()){
+            Toast.makeText(applicationContext,
+            "Title cannot be empty.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        val notes = binding.notesEditText.text.toString().trim()
+        if(notes.isEmpty()){
+            Toast.makeText(applicationContext,
+            "Notes cannot be empty.", Toast.LENGTH_LONG).show()
+            return
+        }
         super.onBackPressed()
     }
 }
